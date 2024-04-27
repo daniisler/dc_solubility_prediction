e5f013bcbfc9fd8f3a0a328981e6392831be8289
davidpwalk <david.p.walk@outlook.de>
2024-04-27 13:11:45 +0200

    substructure filtration functions can now take multiple substructures as input

--- filtration_functions.py
+++ filtration_functions.py
@@ -15,6 +15,7 @@ def filter_weight(df, weight, get_larger_values=True):
         return df[df['weight'] >= weight]
     return df[df['weight'] <= weight]
 
+
 def filter_solubility(df, solubility, get_larger_values=True):
     """Filter given dataframe based on given solubility (get rows with larger/smaller solubility)
 
@@ -26,6 +27,7 @@ def filter_solubility(df, solubility, get_larger_values=True):
     if get_larger_values:
         return df[df['Solubility'] >= solubility]
     return df[df['Solubility'] <= solubility]
+
 
 def filter_temperature(df, temperature, get_larger_values=True):
     """Filter given dataframe based on given solubility (get rows with larger/smaller solubility)
@@ -39,6 +41,7 @@ def filter_temperature(df, temperature, get_larger_values=True):
         return df[df['T,K'] >= temperature]
     return df[df['T,K'] <= temperature]
 
+
 def filter_solvent(df, solvent, get_same_solvent=True):
     """Filter given dataframe based on given solvent (get rows with given solvent or rows with different solvent)
 
@@ -51,9 +54,10 @@ def filter_solvent(df, solvent, get_same_solvent=True):
         df = df[df['Solvent'] == solvent]
         if df.empty:
             print('Solvent not found in database')
-            return df # not sure if return 0 would be better
+            return df  # not sure if return 0 would be better
         return df
     return df[df['Solvent'] != solvent]
+
 
 def filter_solvent_smiles(df, smiles_solvent, get_same_solvent=True):
     """Filter given dataframe based on given smiles of solvent (get rows with given solvent or rows with different solvent)
@@ -67,51 +71,79 @@ def filter_solvent_smiles(df, smiles_solvent, get_same_solvent=True):
         df = df[df['SMILES_Solvent'] == smiles_solvent]
         if df.empty:
             print('Solvent not found in database')
-            return df # not sure if return 0 would be better
+            return df  # not sure if return 0 would be better
         return df
     return df[df['SMILES_Solvent'] != smiles_solvent]
 
-def filter_molecule_substructure(df, smiles_substructure, is_substructure_in_molecule=True):
-    """Filter given dataframe based on given smiles of substructure (get rows where given substructure is part of the molecule,
-    if is_substructure_in_molecule is True)
+
+def filter_molecule_substructure(df, smiles_substructures, is_substructure_in_molecule=True):
+    """Filter molecules based on given list of smiles of substructures (get rows where given substructures are part of
+    the molecule, if is_substructure_in_molecule is True)
 
     :param dataframe df: dataframe of input data
-    :param str smiles_substructure: smiles of substructure used for filtering
-    :param bool is_substructure_in_molecule: True if values rows with given solvent should be returned, false otherwise
+    :param list of strings smiles_substructures: smiles of substructure(s) used for filtering
+    :param bool is_substructure_in_molecule: True if values rows with given molecule should be returned, false otherwise
     :return: filtered dataframe
     """
-    mol_substructure = Chem.MolFromSmiles(smiles_substructure)
-    PandasTools.AddMoleculeColumnToFrame(df, smilesCol='SMILES', molCol='mol_molecule') # should be removed if df already has molecule column
-    mask = []
-    for mol in df['mol_molecule']:
-        mask.append(mol.HasSubstructMatch(mol_substructure))
-    df = df.drop(['mol_molecule'], axis=1) # should be removed if df already has molecule column
+    mol_substructures = []
+    for smiles_sub in smiles_substructures:
+        mol_substructures.append(Chem.MolFromSmiles(smiles_sub))
+    PandasTools.AddMoleculeColumnToFrame(df, smilesCol='SMILES',
+                                         molCol='mol_molecule')  # should be removed if df already has molecule column
     if is_substructure_in_molecule:
-        return df[mask]
-    return df[[not elem for elem in mask]] # round about way to flip booleans in mask
+        for mol_sub in mol_substructures:
+            mask = []
+            for mol_mol in df['mol_molecule']:
+                mask.append(mol_mol.HasSubstructMatch(mol_sub))
+            df = df[mask]
+    else:
+        for mol_sub in mol_substructures:
+            mask = []
+            for mol_mol in df['mol_molecule']:
+                mask.append(mol_mol.HasSubstructMatch(mol_sub))
+            df = df[[not elem for elem in mask]]  # round about way to flip booleans in mask
+    return df.drop(['mol_molecule'], axis=1)
 
-def filter_solvent_substructure(df, smiles_substructure, is_substructure_in_solvent=True):
-    """Filter given dataframe based on given smiles of substructure (get rows where given substructure is part of the solvent,
-    if is_substructure_in_solvent is True)
+
+def filter_solvent_substructure(df, smiles_substructures, is_substructure_in_solvent=True):
+    """Filter solvents based on given list of smiles of substructures (get rows where given substructures are part of
+    the solvent, if is_substructure_in_solvent is True)
 
     :param dataframe df: dataframe of input data
-    :param str smiles_substructure: smiles of substructure used for filtering
+    :param list of strings smiles_substructures: smiles of substructure(s) used for filtering
     :param bool is_substructure_in_solvent: True if values rows with given solvent should be returned, false otherwise
     :return: filtered dataframe
     """
-    mol_substructure = Chem.MolFromSmiles(smiles_substructure)
-    df = df[~(df['SMILES_Solvent'] == '-')] # Remove rows without smiles for solvent
+    mol_substructures = []
+    for smiles_sub in smiles_substructures:
+        mol_substructures.append(Chem.MolFromSmiles(smiles_sub))
+    df = df[~(df['SMILES_Solvent'] == '-')]  # Remove rows without smiles for solvent
     PandasTools.AddMoleculeColumnToFrame(df, smilesCol='SMILES_Solvent', molCol='mol_solvent')
-    mask = []
-    for mol_sol in df['mol_solvent']:
-        mask.append(mol_sol.HasSubstructMatch(mol_substructure))
-    df = df.drop(['mol_solvent'], axis=1) # should be removed if df already has molecule column
     if is_substructure_in_solvent:
-        return df[mask]
-    return df[[not elem for elem in mask]] # round about way to flip booleans in mask
+        for mol_sub in mol_substructures:
+            mask = []
+            for mol_sol in df['mol_solvent']:
+                mask.append(mol_sol.HasSubstructMatch(mol_sub))
+            df = df[mask]
+    else:
+        for mol_sub in mol_substructures:
+            mask = []
+            for mol_sol in df['mol_solvent']:
+                mask.append(mol_sol.HasSubstructMatch(mol_sub))
+            df = df[[not elem for elem in mask]]  # round about way to flip booleans in mask
+    return df.drop(['mol_solvent'], axis=1)
+
+def filter_solvent_heteroatoms(df, heteroatoms_in_solvent=False):
+    """Filter molecules based on whether they contain heteroatoms or not.
+
+    :param dataframe df: dataframe of input data
+    :param bool heteroatoms_in_solvent: False if solvents with heteroatoms should be removed, True otherwise
+    :return: filtered dataframe
+    """
+    return filter_solvent_substructure(df, ['B', 'O', 'N', 'S', 'P', 'F', 'Cl', 'Br', 'I'], heteroatoms_in_solvent)
+
 
 # Ideas for filter functions for solvent:
-#   Polar/Non-Polar (check if molecule has heteroatoms, sadly did not find an easy way to calculate)
 #   Functional Group (check name of solvent)
 #   H bond acceptor/H bond acceptor and donor
 #   Aromatic or not (there is a rdkit function to check this)
