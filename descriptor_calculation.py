8b2a7abbc6fff6ba090b2af7f253f17aa26f61cd
daniisler <isler.dani@gmx.ch>
2024-04-29 08:57:00 +0200

    cleanup and integrate exception handling

--- descriptor_calculation.py
+++ descriptor_calculation.py
@@ -2,7 +2,7 @@
 ### Detrmine Descriptors
 """
     this scripts calculates the dipole moment of all compounds within the cured data set.
-    It therefor uses RDkit to calculate some conformers of each compound. The boltzmann averaged dipolemoment of the conformer ensemble is calculated
+    It therefor uses RDkit to calculate some conformers of each compound. The Boltzmann averaged dipole moment of the conformer ensemble is calculated
 
     Calculated descriptors: 
     - Dipole
@@ -23,212 +23,128 @@
 
 ### Import needed packages:______________________________________________________________________
 import os
-
-## The following code copied from Google colab. Not compatibel with py script in VisStudioCode??
-'''
-# Import of needed functions in the terminal:
-%%bash
-
-# Install some python packages that we use throughout the exercise
-pip install rdkit
-pip install morfeus-ml
-pip install pyberny
-pip install qcengine
-pip install xtb
-
-# Could improve by using crest...
-#Download miniforge, our way of installing conda
-#wget -c https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
-#chmod +x Miniforge3-Linux-x86_64.sh
-#bash ./Miniforge3-Linux-x86_64.sh -b -f -p /usr/local
-
-# Use conda to install CREST
-#conda install -q -y -c conda-forge crest
-'''
-
-# Tried via terminal for vsc: python -c "from ??? import ???"
-'''
+import sys
+from logger import logger
 import pandas as pd
 import numpy as np
 from rdkit import Chem
-from rdkit.Chem import AllChem
-from rdkit.Chem.rdmolfiles import MolToXYZFile
-from morfeus.conformer import ConformerEnsemble
+from rdkit.Chem import AllChem, Draw
 from morfeus import XTB
+from morfeus.conformer import ConformerEnsemble
+from rdkit.Chem.rdmolfiles import MolToXYZFile
+from rdkit.Chem.Lipinski import NumHAcceptors, NumHDonors, NumAromaticRings
 
-# to calculate some describtors: https://www.rdkit.org/docs/source/rdkit.Chem.Lipinski.html#rdkit.Chem.Lipinski.NumHAcceptors
-from rdkit.Chem.Lipinski import NumHAcceptors
-from rdkit.Chem.Lipinski import NumHDonors
-from rdkit.Chem.Lipinski import NumAromaticRings
-'''
+# Env
+PROJECT_ROOT = os.path.dirname(__file__)
+DATA_DIR = os.path.join(PROJECT_ROOT, 'input_data')
+logger = logger.getChild('descriptor_calculation')
+input_file = os.path.join(DATA_DIR, 'BigSolDB_filtered.csv')
 
 ### Data import: _________________________________________________________________________________
-# not sure if this works...
-PROJECT_ROOT = os.path.dirname(os.path.abspath("input_data/.combined_cleaned_data.csv.icloud"))
-df = os.path.join(PROJECT_ROOT,'cure')
+df = pd.read_csv(input_file)
 
-## via google colab: 
-# here choose dataset with water as solvent from polybox (manually uploaded)
-df_aq = pd.read_csv('combined_cleaned_data_aq.csv')
+# for example the following smiles throws an error in qcengine (likely structure optimization doesn't converge)
+excluded_smiles = 'Brc1cc(Br)cc(CN2CCSCC2)c1'
+# Display the problematic molecule
+# molecule = Chem.MolFromSmiles(excluded_smiles)
+# img = Draw.MolToImage(molecule)
+# img.show()
 
-# following smiles has problem (comment below)
-target_smiles = 'Brc1cc(Br)cc(CN2CCSCC2)c1'
-# remove smiles which gives errormessage:
-df = df_aq.drop(df_aq[df_aq['smiles'] == target_smiles].index, axis=0)
+# TODO: Remove me, only for testing!
+# df = df[:5]
 
-print(len(df_aq))
-print(len(df))
-
-'''...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-### ask stephan!
-
-Problem with the following smies:
-Error encountered for molecule with SMILES: Brc1cc(Br)cc(CN2CCSCC2)c1
- (found yusing debugging in code)
-
-'''
-# Find rows with the specific SMILES string
-target_smiles = 'Brc1cc(Br)cc(CN2CCSCC2)c1'
-matches = df[df['smiles'] == target_smiles]
-
-# Print the matching rows
-print(matches)
-# show molecule:
-# Convert the SMILES into an RDKit Mol object
-molecule = Chem.MolFromSmiles(target_smiles)
-
-# Display the Mol object
-molecule
-
-# try to calc conf ensemble:
-rdkit_value_target_smiles = ce_from_rdkit(target_smiles)#will give errormessage; runtime 27s
-
-'''...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'''
-
-
-### Functions:____________________________________________________________________________________________________________________
+### Functions:__________________________________________________________________________________________________
 def ce_from_rdkit(smiles):
     """
-    This functions generates random conformers beased on the smiles
-    and gets rid of redunant conformers based on RMSD. The remaining conformers
+    This functions generates random conformers based on the smiles
+    and gets rid of redundant conformers based on RMSD. The remaining conformers
     are optimised using xtb on a level of GFN-FF (a force-field). The energy
     of the optimised geometry is determined with xtb on a level of GFN2-xTB.
     The energetically most favorable conformers are then selected.
 
 
-    Mostly took from ex. 4
+    Adapted from ex. 4
     """
     # Generate MORFEUS Conformer Ensemble from RDKit and prune by RMSD
-    ce_rdkit = ConformerEnsemble.from_rdkit(smiles, optimize="MMFF94")
-    ce_rdkit.prune_rmsd()
+    # MMFF94s reflects the time averaged structure better, which is what we need
+    try:
+        ce_rdkit = ConformerEnsemble.from_rdkit(smiles, n_confs=10, optimize="MMFF94s")
+        ce_rdkit.prune_rmsd()
+    except Exception as e:
+        logger.error(f"While generating conformers for {smiles}: {e}")
+        return 'failed'
 
     # Optimise all of the remaining conformers and sort them energetically
-    model={"method": "GFN-FF"}
-    ce_rdkit.optimize_qc_engine(program="xtb", model=model, procedure="berny")
-    ce_rdkit.sort()
+    try:
+        model={"method": "GFN-FF"}
+        ce_rdkit.optimize_qc_engine(program="xtb", model=model, procedure="berny")
+        ce_rdkit.sort()
+    except Exception as e:
+        logger.error(f"While optimizing conformers for {smiles}: {e}")
+        return 'failed'
 
     # Single point energy calculation and final energetic sorting
-    model={"method": "GFN2-xTB"}
-    ce_rdkit.sp_qc_engine(program="xtb", model=model)
-    ce_rdkit.sort()
+    try:
+        model={"method": "GFN2-xTB"}
+        ce_rdkit.sp_qc_engine(program="xtb", model=model)
+        ce_rdkit.sort()
+    except Exception as e:
+        logger.error(f"While calculating single point energy for {smiles}: {e}")
     # Generate molecule representation
     #ce_rdkit.generate_mol()
-
+    logger.info(f"Conformer ensemble for molecule with SMILES: {smiles} generated successfully")
     return ce_rdkit
-
 
 # Function to generate bolzman average dipole from conformer ensemble
 def get_dipole(ce):
-  for conformer in ce:
-    xtb = XTB(conformer.elements, conformer.coordinates)
-    dipole = xtb.get_dipole()# gives array as 3D vector
-    conformer.properties["dipole"] = np.linalg.norm(dipole)
+    for conformer in ce:
+        xtb = XTB(conformer.elements, conformer.coordinates)
+        dipole = xtb.get_dipole() # gives array as 3D vector
+        conformer.properties["dipole"] = np.linalg.norm(dipole)
 
-  return ce.boltzmann_statistic("dipole")
+    return ce.boltzmann_statistic("dipole")
 
 # Get molecular structure using rdkit
 def get_mol(smiles, get_Hs = True):
-  mol = Chem.MolFromSmiles(smiles)
-  mol_hasHs = Chem.AddHs(mol)
-  
-  if get_Hs:
-    return(mol_hasHs)
-  else:
-    return(mol)
+    mol = Chem.MolFromSmiles(smiles)
+    mol_hasHs = Chem.AddHs(mol)
 
-# Get some further descriptors from rdkit
-def get_HAcceptors(mol): 
-  HBA = NumHAcceptors(mol)#CalcNumLipinskiHBA(mol)
-  return(HBA)
+    if get_Hs:
+        return(mol_hasHs)
+    else:
+        return(mol)
 
-def get_HDonors(mol): 
-  HBD = NumHDonors(mol)
-  return(HBD)
-
-def get_AromaticRings(mol):
-  return(NumAromaticRings(mol))
-
-
-### Get Descriptors ______________________________________________________________________________________________________________
-# Initialisations:
-conf_ensemble_rdkit = []
-mol_rdkit = []
-HBA_rdkit = []
-HBD_rdkit = []
-AromatRi_rdkit = []
-
-
-##...Add Conformer ensemble:
-for index, row in df.iterrows():# itterate over rows
-
-    # Get the ConformerEnsemble from ce_from_crest
-    rdkit_value = ce_from_rdkit(row['smiles'])
-    # Append the value to the list
-    conf_ensemble_rdkit.append(rdkit_value)
-    # for debugging: 
-    '''
-    try:
-      rdkit_value = ce_from_rdkit(row['smiles'])
-      # Append the value to the list
-      conf_ensemble_rdkit.append(rdkit_value)
-    except Exception as e:
-      print(f"Error encountered for molecule with SMILES: {row['smiles']}")
-      raise e  # Re-raise the exception for further handling 
-      '''
-
-# Assign the list as a new column in the acids DataFrame
-df['ensemble_rdkit'] = conf_ensemble_rdkit
-
-##...get dipole from conformer in new column: 
-df['dipole'] = df['ensemble_crest'].apply(get_dipole)
-
-
-##...Add Molecular Structure:
+### Calculate Descriptors ______________________________________________________________________________
+conf_ensemble_rdkit = {}
+# Calculate conformer ensemble for all molecules to obtain the dipole moments
 for index, row in df.iterrows():
+    if row['SMILES'] not in conf_ensemble_rdkit.keys():
+        logger.info(f"Calculating conformer ensemble for molecule with SMILES: {row['SMILES']}")
+        ce_rdkit = ce_from_rdkit(row['SMILES'])
+        conf_ensemble_rdkit[row['SMILES']] = ce_rdkit
+    # TODO: Remove me, only for testing!
+    if index > 150:
+        break
+# Add the conformer ensemble to the dataframe
+df['ensemble_rdkit'] = df['SMILES'].apply(lambda x: conf_ensemble_rdkit[x] if x in conf_ensemble_rdkit.keys() else 'failed')
+# Extract the molecules with failed conformer calculation
+failed_molecules = df[df['ensemble_rdkit'] == 'failed']
+df = df.drop(failed_molecules.index, axis=0)
 
-    # Get molecular structure
-    rdkit_mol = get_mol(row['smiles'], True)
-    # Append mol to the list
-    mol_rdkit.append(rdkit_mol)
+logger.info('Calculating dipole moment for all successfully generated conformers')
+
+df['dipole'] = df['ensemble_rdkit'].apply(get_dipole)
+
+# Add the mol structure to the dataframe -> TODO: Kind of redundant, as we already have the ce?
+df['mol_structure'] = df.apply(lambda x: get_mol(x['SMILES'], True), axis=1)
+
+logger.info(f"Calculating rdkit descriptors for molecule with SMILES: {row['SMILES']}")
 
 # Assign the list as a new column in the DataFrame
-df['mol_structure'] = mol_rdkit
+df['HBAcceptor'] = df['mol_structure'].apply(NumHAcceptors)
+df['HBDonor'] = df['mol_structure'].apply(NumHDonors)
+df['AromaticRings'] = df['mol_structure'].apply(NumAromaticRings)
 
-
-##...Add rdkit Descriptors:
-for index, row in df.iterrows():
-
-    # Get the Descriptors for each row
-    rdkit_HBA = get_HAcceptors(row['mol_structure'])
-    rdkit_HBD = get_HAcceptors(row['mol_structure'])
-    rdkit_AromatRi = get_AromaticRings(row['mol_structure'])
-
-    # Append the value to the list
-    HBA_rdkit.append(rdkit_HBA)
-    HBD_rdkit.append(rdkit_HBD)
-    AromatRi_rdkit.append(rdkit_AromatRi)
-
-# Assign the list as a new column in the DataFrame
-df['HBAcceptor'] = HBA_rdkit
-df['HBDonor'] = HBD_rdkit
-df['AromaticRings'] = AromatRi_rdkit
+# Save the dataframe -> TODO: Most columns will be useless, as they just point to a (not existing) object
+df.to_csv(os.path.join(DATA_DIR, 'BigSolDB_filtered_descriptors.csv'), index=False)
+failed_molecules.to_csv(os.path.join(DATA_DIR, 'BigSolDB_filtered_failed.csv'), index=False)
