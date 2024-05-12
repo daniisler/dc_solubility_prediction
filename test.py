3965c5f0f388a681f83ef583e2e79812c6c02ee2
kesslece <celeste.kessler@gmail.com>
2024-05-12 15:14:13 +0200

    calc conf..

--- test.py
+++ test.py
@@ -129,7 +129,7 @@ dipole_dict = {}
 if smiles_test not in conf_ensemble_rdkit.keys():
     try:
         logger.info(f"Calculating conformer ensemble for molecule with SMILES:{smiles_test}")
-        ce_from_rdkit(smiles_test)
+        ce_rdkit = ce_from_rdkit(smiles_test)
         
         # Check if the conformer ensemble has already been calculated
         # smiles_identifier = smiles_to_file(smiles_test)
