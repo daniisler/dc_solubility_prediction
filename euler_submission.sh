8ee5560691c61b5dd39c9c86558fb0e08a015183
daniisler <isler.dani@gmx.ch>
2024-05-14 13:47:21 +0200

    updated to reasonable parameters

--- euler_submission.sh
+++ euler_submission.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=dc_solubility        #Name of the job
-#SBATCH --ntasks=1            #Requesting 1 node (is always 1)
-#SBATCH --cpus-per-task=128     #Requesting 1 CPU
-#SBATCH --mem-per-cpu=1G      #Requesting 4 Gb memory per core
-#SBATCH --time=4:00:00        #Requesting 4 hours running time
-#SBATCH --output logs/main.log      #Log
+#SBATCH --job-name=dc_solubility    #Name of the job
+#SBATCH --ntasks=1                  #Requesting 1 node (is always 1)
+#SBATCH --cpus-per-task=128         #Requesting 128 CPU
+#SBATCH --mem-per-cpu=1G            #Requesting 1 Gb memory per core
+#SBATCH --time=50:00:00             #Requesting 50 hours running time
+#SBATCH --output logs/main.log      #Log-file
 
 
 #Change to the new software stack
@@ -16,5 +16,5 @@ module load gcc/8.2.0 python/3.11.2 eth_proxy
 #Set environment variables
 export NUMEXPR_MAX_THREADS=128
 
-#The bcftools command
+#Run the python script
 python main.py
